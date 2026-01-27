e357bb0d698f042913581aba45df7a293743d479
Mazà Faniriniaina <faniryranaivoson167@gmail.com>
2026-01-27 16:23:43 +0300

    Added build script and libs

---
+++ build.bat
@@ -0,0 +1,6 @@
+mvn install:install-file ^
+  -Dfile="C:\Users\ME-PC\Documents\GitHub\ProjetFrameworkS5\backoffice\libs\framework-1.0-SNAPSHOT.jar" ^
+  -DgroupId=com.itu.framework ^
+  -DartifactId=framework ^
+  -Dversion=1.0-SNAPSHOT ^
+  -Dpackaging=jar

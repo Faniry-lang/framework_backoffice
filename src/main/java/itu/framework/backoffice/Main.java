17c56da2065bb42bedb12c7cfd19d76d0d77ed14
manjaka andriantsoa <163170414+Manjaka-dev@users.noreply.github.com>
2026-01-28 14:58:04 +0300

    Add Jetty server setup and update project configuration
    (cherry picked from commit 0a6f653049ae605b9c6346068fe700707bbc2e04)

--- src/main/java/itu/framework/backoffice/Main.java
+++ src/main/java/itu/framework/backoffice/Main.java
@@ -1,17 +1,25 @@
 package itu.framework.backoffice;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int port = Integer.parseInt(System.getenv("PORT"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Server server = new Server(port);
+
+        ServletContextHandler context =
+                new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+        context.setContextPath("/");
+
+        server.setHandler(context);
+        server.start();
+        server.join();
     }
 }

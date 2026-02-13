e41f14e14c92d028aa571248f5cb40bb0d5d8790
manjaka andriantsoa <163170414+Manjaka-dev@users.noreply.github.com>
2026-02-13 08:47:02 +0300

    feat: implement TokenClient entity with token validation logic and update application properties

--- src/main/java/itu/framework/backoffice/entities/TokenClient.java
+++ src/main/java/itu/framework/backoffice/entities/TokenClient.java
@@ -2,22 +2,73 @@ package itu.framework.backoffice.entities;
 
 import legacy.annotations.Column;
 import legacy.annotations.Entity;
-import legacy.annotations.Generated;
 import legacy.annotations.Id;
-import legacy.strategy.GeneratedAfterPersistence;
+import legacy.query.Comparator;
+import legacy.query.FilterSet;
+import legacy.schema.BaseEntity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity(tableName = "token_client")
-public class TokenClient {
+public class TokenClient extends BaseEntity {
+    public TokenClient() {
+        super();
+    }
+
     @Id
     @Column
-    @Generated(strategy = GeneratedAfterPersistence.class)
-    Integer id;
+    private Long id;
 
-    @Column(name = "token")
-    String token;
+    @Column
+    private String token;
 
-    @Column(name = "date_expiration")
-    LocalDateTime dateExpiration;
+    @Column(name = "expiration_date")
+    private LocalDateTime expirationDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public LocalDateTime getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDateTime expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public static boolean isTokenValid(String token, LocalDateTime now) {
+        if (token == null || token.isEmpty()) {
+            return false;
+        }
+
+        try {
+            List<TokenClient> tokenClients = new ArrayList<>();
+            FilterSet filterSet = new FilterSet();
+            if (now != null ) {
+                filterSet.add("token", Comparator.EQUALS, token);
+                filterSet.add("expirationDate", Comparator.GREATER_THAN, now);
+                tokenClients = TokenClient.filter(TokenClient.class, filterSet);
+            }
+            return !tokenClients.isEmpty();
+        } catch (Exception e) {
+            System.err.println("Erreur lors de la verification du token:" + e.getMessage());
+            return false;
+        }
+    }
+
 }
---
+++ src/test/java/TestToken.java
@@ -0,0 +1,5 @@
+public class TestToken {
+    public static void main(String[] args) {
+
+    }
+}

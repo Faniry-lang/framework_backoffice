1e42ddf1462871312a9a95176f4d30fa24c57f40
Mazà Faniriniaina <faniryranaivoson167@gmail.com>
2026-03-18 09:57:56 +0300

    Create GroupeReservation.java

---
+++ src/main/java/itu/framework/backoffice/models/GroupeReservation.java
@@ -0,0 +1,27 @@
+package itu.framework.backoffice.models;
+
+import itu.framework.backoffice.entities.Reservation;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class GroupeReservation {
+    List<Reservation> reservations;
+    LocalDateTime heureArriveeVehicule;
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+
+    public LocalDateTime getHeureArriveeVehicule() {
+        return heureArriveeVehicule;
+    }
+
+    public void setHeureArriveeVehicule(LocalDateTime heureArriveeVehicule) {
+        this.heureArriveeVehicule = heureArriveeVehicule;
+    }
+}
